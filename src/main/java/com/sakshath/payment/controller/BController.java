5687b733b9ed47bff6ba1f8782b13e3899513b43
gouravSakshath <gouravbisht786@gmail.com>
2024-08-03 23:46:05 +0530

    we have com

--- src/main/java/com/sakshath/payment/controller/BController.java
+++ src/main/java/com/sakshath/payment/controller/BController.java
@@ -3,12 +3,9 @@ package com.sakshath.payment.controller;
 public class BController {
 private void syso() {
 	// TODO Auto-generated method stub
-//hellllll
-<<<<<<< HEAD
+
 	
 	System.out.println("last me");
-=======
-	//khkhkhkhkhkh
->>>>>>> 6f0d0a273bdb4eecb579003c45cb85493277190a
+
 }
 }
