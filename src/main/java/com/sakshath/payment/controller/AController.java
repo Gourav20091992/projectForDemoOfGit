f6b84ff065213e139f0c8276373cdba78a6c5eca
Durgesh <gouravbisht786@gmail.com>
2024-08-03 16:22:10 +0530

    second commit for controllerA

--- src/main/java/com/sakshath/payment/controller/AController.java
+++ src/main/java/com/sakshath/payment/controller/AController.java
@@ -1,5 +1,17 @@
 package com.sakshath.payment.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AController {
+	
+	
+	public void m1() {
+		
+		System.out.println("Hi this is gourav");
+	}
+	
 
 }
